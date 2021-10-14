c048747b898e6c544df0f127ad577a3418781c63
Pablo Dulibine <dulibine.pablo@gmail.com>
2021-10-14 11:09:40 +0200

    ajout projet

---
+++ timeserie/src/main/java/com/timeserie/timeserie/TimeserieApplication.java
@@ -0,0 +1,13 @@
+package com.timeserie.timeserie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeserieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeserieApplication.class, args);
+	}
+
+}
---
+++ timeserie/src/test/java/com/timeserie/timeserie/TimeserieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.timeserie.timeserie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeserieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
